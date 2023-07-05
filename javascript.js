0efe0f847132d8c0b27bb08426a1f3cb1f11d7a4
Oscar F <[200~133872824+olf01@users.noreply.github.com>
2023-07-05 18:19:28 +0100

    Add color settings and update css

--- javascript.js
+++ javascript.js
@@ -4,9 +4,16 @@ const clearCanvasButton = document.getElementById('clearCanvas')
 const gridSizeDisplay = document.getElementById('gridSizeDisplay')
 const drawButton = document.getElementById('draw')
 const eraseButton = document.getElementById('erase')
-let gridSize = 8
+const penColorInput = document.getElementById('penColor')
+const gridBackgroundColorInput = document.getElementById('gridBackgroundColor')
+const unicornPenColorInput = document.getElementById('unicornPenColor')
+
+let gridSize = 16
 const MIN_GRID_SIZE = 2
 const MAX_GRID_SIZE = 128
+
+let penColor = 'black'
+let gridBackgroundColor = 'white'
 
 function updateGridSize (size) {
     if (size < MIN_GRID_SIZE) {
@@ -14,11 +21,12 @@ function updateGridSize (size) {
     } else if (size > MAX_GRID_SIZE) {
         gridSize = MAX_GRID_SIZE;
     } else {
-        gridSize = size;
+        gridSize = Math.floor(size);
     }
 }
 
 function createGrid (size) {
+    changeBackgroundColor(gridBackgroundColor);
     grid.innerHTML = "";
     updateGridSize(size);
     gridSizeDisplay.textContent = gridSize
@@ -40,13 +48,36 @@ createGrid(gridSize)
 
 gridSizeInput.addEventListener('input', function () {createGrid(this.value)});
 
+
+
+function clearCanvas () {
+    document.querySelectorAll('.cell').forEach(function (cell) {
+        cell.style.backgroundColor = 'transparent'
+    })
+}
+
+clearCanvasButton.addEventListener('click', clearCanvas)
+
+
+
+function changeBackgroundColor (color) {
+    gridBackgroundColor = color;
+    grid.style.backgroundColor = color;
+}
+
+gridBackgroundColorInput.addEventListener('input', function () {changeBackgroundColor(this.value)})
+
+
+
 let drawMode = 'draw'
 
 function drawCell (e) {
     if (drawMode == 'draw') {
-        e.target.style.backgroundColor = 'red';
+        e.target.style.backgroundColor = penColor;
+    } else if (drawMode == 'unicorn') {
+        e.target.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
     } else if (drawMode == 'erase') {
-        e.target.style.backgroundColor = 'azure';
+        e.target.style.backgroundColor = 'transparent';
     }
 }
 
@@ -55,18 +86,34 @@ function mousedownHandler (e) {
     drawCell(e); 
     document.querySelectorAll('.cell').forEach(function (cell) {cell.addEventListener('mouseenter', drawCell)})
 }
+
 grid.addEventListener('mousedown', mousedownHandler)
+
 function mouseupHandler () {
     document.querySelectorAll('.cell').forEach(function (cell) {cell.removeEventListener('mouseenter', drawCell)})
 }
+
 document.addEventListener('mouseup', mouseupHandler)
 
-drawButton.addEventListener('click', function () {drawMode = 'draw'})
-eraseButton.addEventListener('click', function () {drawMode = 'erase'})
 
-function clearCanvas () {
-    document.querySelectorAll('.cell').forEach(function (cell) {
-        cell.style.backgroundColor = 'azure'
-    })
+
+function unpressButtons () {
+    document.querySelectorAll('.pressed').forEach(function (button) {button.classList.remove('pressed')})
 }
-clearCanvasButton.addEventListener('click', clearCanvas)
+
+
+
+drawButton.addEventListener('click', function () {unpressButtons(); drawMode = 'draw'; this.classList.add('pressed');})
+eraseButton.addEventListener('click', function () {unpressButtons(); drawMode = 'erase'; this.classList.add('pressed');})
+
+
+
+function changePenColor (color) {
+    penColor = color;
+}
+
+penColorInput.addEventListener('input', function () {changePenColor(this.value)})
+
+
+
+unicornPenColorInput.addEventListener('click', function () {unpressButtons(); drawMode = 'unicorn'; this.classList.add('pressed');})
